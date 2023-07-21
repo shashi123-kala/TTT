0127933e205cb5d4a73102844c0e89a9a9e14814
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-21 16:59:28 +0530

    Add: Initialize TicTacToe game

--- src/main/java/com/ttt/tdd/TicTacToe.java
+++ src/main/java/com/ttt/tdd/TicTacToe.java
@@ -1,15 +1,31 @@
 package com.ttt.tdd;
 
+import com.ttt.exception.TicTacToeException;
+
 public class TicTacToe {
-	
+
 	private Character[][] board = { { '\0', '\0', '\0' }, { '\0', '\0', '\0' }, { '\0', '\0', '\0' } };
+	private char lastPlayer = '\0';
 
 
-	public Character play(int row, int column, Character player ) {
-       if (player == 'X') {
-    	   return  board[column - 1][row - 1] = 'X';
-		}
-       return  board[column - 1][row - 1] = 'O';
+	public void play(int row, int column/* , Character player */) {
+		if (board[column - 1][row - 1] != '\0') {
+            throw new RuntimeException("Field is occupied!");
+        } else {
+            board[column - 1][row - 1] = 'X';
+        }
+		
+//		if (column < 3) {
+//            throw new RuntimeException("Field is occupied!");
+//        }
+		lastPlayer = nextPlayer();
+	}
+
+	public char nextPlayer() {
+		if (lastPlayer == 'X') {
+            return 'O';
+        }
+        return 'X';
 	}
 
 }
--- src/test/java/com/ttt/tdd/TicTacToeTest.java
+++ src/test/java/com/ttt/tdd/TicTacToeTest.java
@@ -3,8 +3,12 @@ package com.ttt.tdd;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+
+import com.ttt.exception.TicTacToeException;
 
 public class TicTacToeTest {
 
@@ -12,6 +16,9 @@ public class TicTacToeTest {
 	//= new TicTacToe();
 	public static final Character FIRST_PLAYER = 'X';
 	public static final Character SECOND_PLAYER = 'O';
+	@Rule
+    public ExpectedException exception = ExpectedException.none();
+
 	
 	 @BeforeEach
 	    public final void before() {
@@ -25,14 +32,39 @@ public class TicTacToeTest {
 
 	@Test
 	public void playWithFIrstPlayer() {
-		Character actual = tictactoe.play(1, 1,'X');
-		assertEquals(FIRST_PLAYER, actual);
+		//Character actual = tictactoe.play(1, 1,'X');
+		//assertEquals(FIRST_PLAYER, actual);
+		 assertEquals('X', tictactoe.nextPlayer());
 	}
+	
+	
+
 	
 	@Test
 	public void playWithSecondPlayer() {
-		Character actual = tictactoe.play(1, 2,'O');
-		assertEquals(SECOND_PLAYER, actual);
+//		Character actual = tictactoe.play(1, 2,'O');
+//		assertEquals(SECOND_PLAYER, actual);
+		tictactoe.play(1,1);
+		assertEquals('O', tictactoe.nextPlayer());
+
 	}
+
+//	@Test
+//	public void RunTimeExceptionFoundWhenXOutsideOfBoard() {
+//		exception.expect(TicTacToeException.class);
+//		tictactoe.play(5, 2,'X');
+//	}
+
+	@Test
+    public void whenOccupiedThenRuntimeException() {
+        exception.expect(TicTacToeException.class);
+        tictactoe.play(2, 1);
+    }
+	
+//	@Test
+//	public void RunTimeExceptionFoundWhenXOutsideOfBoard() {
+//		exception.expect(TicTacToeException.class);
+//		tictactoe.play(6, 5);
+//	}
 
 }
